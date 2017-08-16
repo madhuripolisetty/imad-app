f6440a86cdeca6a399d220b1820afe553a3da2d3
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-16 21:47:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/articl-one', function (req, res) {
+  res.send('article -one is requested and will be served here');
+});
+
+app.get('/articl-two', function (req, res) {
+  res.send('article -two is requested and will be served here');
+});
+
+app.get('/articl-three', function (req, res) {
+  res.send('article -three is requested and will be served here');
+});
+
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
