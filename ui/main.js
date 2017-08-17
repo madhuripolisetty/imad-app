d68996d49cf53801e90696d96f73a423ef8e0cb9
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-17 17:34:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,18 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new content from main.js';
+
+//move the image as animation
+
+var marginLeft = 0;
+var img1 =  document.getElementById('madi');
+function moveRight ()
+{
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img1.onclick =  function ()
+                {
+                    var interval = setInterval(moveRight, 50);
+                };
