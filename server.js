baed0a6f1c2024d3bceae947164408862034ce0c
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-17 14:24:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ function createTemplate (data)
                                 
                                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                                 <link href="/ui/style.css" rel="stylesheet" />
-                            <head>
+                            </head>
                             
                             <body>
                              <div class="container" >  
