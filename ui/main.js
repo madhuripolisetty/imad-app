45e38a4ddd4a04641b701d2e1ab24cbdd9ad2af3
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-21 00:11:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,6 @@ var counter = 0;
 button.onclick = function ()
                   { 
                     counter = counter + 1;
-                    var span = document.getElementById('span');
+                    var span = document.getElementById('count');
                     span.innerHTML = counter.toString();
                   };
