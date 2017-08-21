8fc8ff03b0ef8a7a9d538b8a15671e4a44768d8d
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-21 20:13:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,11 +126,21 @@ app.get('/counter', function (req, res)
 });
 
 
+
+
+
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
+var names = [];
+app.get('/submit-name/:name', function (req, res)
+{
+  var name = req.params.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
 
 
 app.get('/ui/style.css', function (req, res) {
