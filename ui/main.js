8d8c5458cab54812038d9ea6bedd15b283f7a5dd
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-22 01:58:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -81,13 +81,13 @@ submit.onclick = function ()
                         var ul =  document.getElementById('namelist');
                         ul.innerHTML = list;
                      }
-                 
+                     
     //begin of changes                             
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 //end of changes
-
+}
 request.open('GET', 'http://mailtomadhurip.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
-}
+
 };
