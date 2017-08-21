9268ec53703246814cb630584212303606e6fa54
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-22 02:24:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -61,7 +61,7 @@ request.send(null);
 
 /*var nameInput = document.getElementById('name');
 var name = nameInput.value;*/
-var submit =  document.getElementById('submit');
+var submit =  document.getElementById('submit_btn');
 submit.onclick = function ()
                  {
                      var request = new XMLHttpRequest();
