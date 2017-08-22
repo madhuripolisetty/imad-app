868164ba4b04e6e25d20b5251397bd1d38b3ab42
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-22 22:14:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ app.get('/counter', function (req, res)
 
 app.get('/articles/:articleName', function (req, res)
 { 
-  Pool.query("SELECT * FROM article WHERE title = '" + request.params.articleName + "'" , function (err, result)
+  Pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'" , function (err, result)
                                                                                           {
                                                                                              if(err)
                                                                                              {
