ff5611d6caedea1d841d6240fab6fd3f1e753d18
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-22 20:53:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 
 var config = {
                 user:      'coco98',
-                database:  'mailtomadhurip',
+                database:  'coco98',
                 host:      'db.imad.hasura-app.io',
                 port:      '5432',
                 password:  process.env.DB_PASSWORD
