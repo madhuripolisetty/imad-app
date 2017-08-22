044d370ceb0b3853e4badf938a3954e84540ec49
madhuripolisetty <30684071+madhuripolisetty@users.noreply.github.com>
2017-08-22 14:28:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,13 +116,7 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter = 0;
-app.get('/counter', function (req, res)
-{
-    counter = counter + 1;
-    res.send(counter.toString());
+  
 });
 
 
@@ -133,6 +127,18 @@ app.get('/submit-name', function (req, res)
   names.push(name);
   res.send(JSON.stringify(names));
 });
+
+
+
+
+var counter = 0;
+app.get('/counter', function (req, res)
+{
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+
 
 
 
